f48672aef03818496ea01139783589801e2d28dd
epsniff <epsniff@gmail.com>
2021-10-30 00:24:12 -0700

    moving direct env calls to usig dotenv

--- src/constants/env.rs
+++ src/constants/env.rs
@@ -1,3 +1,4 @@
 pub const HOST: &'static str = "HOST";
 pub const PORT: &'static str = "PORT";
 pub const LOG_LEVEL: &'static str = "LOG_LEVEL";
+pub const CANISTER_PATH: &'static str = "CANISTER_PATH";
