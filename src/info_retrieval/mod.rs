ef5c76678783eef422ebcecdc20478acde0f95b9
AJ Roetker <aj.roetker@lytics.com>
2021-10-29 17:11:28 -0700

    Get canister path from environment variable

--- src/info_retrieval/mod.rs
+++ src/info_retrieval/mod.rs
@@ -3,13 +3,16 @@ pub mod index_canister;
 pub mod local_shard;
 pub mod new_index;
 pub mod types;
+
 use lazy_static::lazy_static;
+use std::path::PathBuf;
+use std::env;
 
 lazy_static! {
     static ref INDEX_CANISTER: index_canister::IndexCanister = index_canister::IndexCanister::new(
-        std::path::PathBuf::from("/Users/ajroetker/projects/rust-camino/canister"),
+        PathBuf::from(env::var("CANISTER_PATH").expect("Failed to find CANISTER_PATH")),
         types::CanisterSettings{
-            base_path: std::path::PathBuf::from("/Users/ajroetker/projects/rust-camino/canister"),
+            base_path: PathBuf::from(env::var("CANISTER_PATH").expect("Failed to find CANISTER_PATH")),
             server_id: 1,
         })
         .expect("Failed to create the global HTTP client instance");
